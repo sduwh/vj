1e852aa50ac39b2308aeaaa29d7739f5a3e36608
张家豪 <davidbill@outlook.com>
2018-11-09 11:14:45 +0800

    修复: 第一名和最后一名特殊判断

--- web/controllers/api/user.py
+++ web/controllers/api/user.py
@@ -6,22 +6,24 @@ import datetime
 class Handler(RequestHandler):
     """
     VJ 成长记录 API
-    """
+    # """
 
     @gen.coroutine
     def get(self):
         params = self._get_params()
         username = self._get_cookie_username()
         user = yield self._find_user_info(username)
+        self.contest_id_list = yield self._get_contest_id_list(username)
         if params['type'] == 'index':
             self.write({
                 'username': user.get('username'),
                 'nickname': user.get('nickname'),
+                'registration_time': (user.get('_id').generation_time.timestamp() * 1000),
                 'total_sub': user.get('total_sub'),
                 'total_ac': user.get('total_ac'),
                 'total_wa': user.get('total_wa'),
                 # 处理last_submit_time为None的情况
-                'last_submit_time': str(user.get('last_submit_time').timestamp() * 1000)
+                'last_submit_time': user.get('last_submit_time').timestamp() * 1000
                 if user.get('last_submit_time') else None,
             })
         elif params['type'] == 'others':
@@ -31,7 +33,7 @@ class Handler(RequestHandler):
                 'last_ac': (yield self._get_last_ac(username)),
                 'diffiult_ac': (yield self._get_difficult_ac(username)),
                 'achievement': (yield self._get_achievement(username)),
-                'contest_list': (yield self._get_contest_list(username)),
+                'contest_list': (yield self._get_contest_list()),
             })
         else:
             self.send_error(500)
@@ -88,19 +90,24 @@ class Handler(RequestHandler):
         last['nickname'] = last_user.get('nickname')
         last['total_ac'] = last_user.get('total_ac')
 
+        # my_rank为None, 1, last_user_rank时处理
+        if not my_rank:
+            my_rank = last_user.get('rank') + 1
         prve = {}
-        prve_user = yield self.settings["database"]["user"].find_one({
-            "rank": my_rank - 1,
-        })
-        prve['nickname'] = prve_user.get('nickname')
-        prve['total_ac'] = prve_user.get('total_ac')
+        if my_rank != 1:
+            prve_user = yield self.settings["database"]["user"].find_one({
+                "rank": my_rank - 1,
+            })
+            prve['nickname'] = prve_user.get('nickname')
+            prve['total_ac'] = prve_user.get('total_ac')
 
         next = {}
-        next_user = yield self.settings["database"]["user"].find_one({
-            "rank": my_rank + 1,
-        })
-        next['nickname'] = next_user.get('nickname')
-        next['total_ac'] = next_user.get('total_ac')
+        if my_rank < last_user.get('rank'):
+            next_user = yield self.settings["database"]["user"].find_one({
+                "rank": my_rank + 1,
+            })
+            next['nickname'] = next_user.get('nickname')
+            next['total_ac'] = next_user.get('total_ac')
 
         other_person['first'] = first
         other_person['last'] = last
@@ -128,18 +135,18 @@ class Handler(RequestHandler):
                 'result': 'Accepted'
             }, sort=[('submittime', 1)]
         )
+        if first_ac:
+            soj = first_ac['soj']
+            sid = first_ac['sid']
+            submission_id = first_ac.get('_id')
+            problem = yield self.settings['database']['problem'].find_one({'soj': soj, 'sid': sid})
+            title = ' {}'.format(problem.get('title')) if problem else ''
+            title = '{}-{}{}'.format(soj, sid, title)
 
-        soj = first_ac['soj']
-        sid = first_ac['sid']
-        submission_id = first_ac.get('_id')
-        problem = yield self.settings['database']['problem'].find_one({'soj': soj, 'sid': sid})
-        title = ' {}'.format(problem.get('title')) if problem else ''
-        title = '{}-{}{}'.format(soj, sid, title)
-
-        ret['time'] = first_ac.get('submittime').strftime('%Y %m %d') if first_ac.get('submittime') else None
-        ret['title'] = title
-        ret['url'] = '/submission/{}'.format(submission_id) if submission_id else None
-        ret['code'] = first_ac.get('code')
+            ret['time'] = first_ac.get('submittime').strftime('%Y %m %d') if first_ac.get('submittime') else None
+            ret['title'] = title
+            ret['url'] = '/submission/{}'.format(submission_id) if submission_id else None
+            ret['code'] = first_ac.get('code')
 
         return ret
 
@@ -162,18 +169,18 @@ class Handler(RequestHandler):
                 'result': 'Accepted'
             }, sort=[('submittime', -1)]
         )
+        if last_ac:
+            soj = last_ac['soj']
+            sid = last_ac['sid']
+            submission_id = last_ac.get('_id')
+            problem = yield self.settings['database']['problem'].find_one({'soj': soj, 'sid': sid})
+            title = ' {}'.format(problem.get('title')) if problem else ''
+            title = '{}-{}{}'.format(soj, sid, title)
 
-        soj = last_ac['soj']
-        sid = last_ac['sid']
-        submission_id = last_ac.get('_id')
-        problem = yield self.settings['database']['problem'].find_one({'soj': soj, 'sid': sid})
-        title = ' {}'.format(problem.get('title')) if problem else ''
-        title = '{}-{}{}'.format(soj, sid, title)
-
-        ret['time'] = last_ac.get('submittime').strftime('%Y %m %d') if last_ac.get('submittime') else None
-        ret['title'] = title
-        ret['url'] = '/submission/{}'.format(submission_id) if submission_id else None
-        ret['code'] = last_ac.get('code')
+            ret['time'] = last_ac.get('submittime').strftime('%Y %m %d') if last_ac.get('submittime') else None
+            ret['title'] = title
+            ret['url'] = '/submission/{}'.format(submission_id) if submission_id else None
+            ret['code'] = last_ac.get('code')
 
         return ret
 
@@ -197,37 +204,38 @@ class Handler(RequestHandler):
                 'result': {'$ne': 'Accepted'}
             }, sort=[('submittime', 1)]
         ).to_list(None)
+        if none_ac_submits:
+            counter = {}
+            for none_ac_submit in none_ac_submits:
+                key = (none_ac_submit['soj'], none_ac_submit['sid'])
+                if not counter.get(key):
+                    counter[key] = 1
+                else:
+                    counter[key] += 1
 
-        counter = {}
-        for none_ac_submit in none_ac_submits:
-            key = (none_ac_submit['soj'], none_ac_submit['sid'])
-            if not counter.get(key):
-                counter[key] = 1
-            else:
-                counter[key] += 1
+            max_none_ac_key = max(counter, key=lambda k: counter[k])
+            soj = max_none_ac_key[0]
+            sid = max_none_ac_key[1]
 
-        max_none_ac_key = max(counter, key=lambda k: counter[k])
-        soj = max_none_ac_key[0]
-        sid = max_none_ac_key[1]
+            difficult_ac = yield self.settings['database']['submission'].find_one(
+                {
+                    'username': username,
+                    'result': 'Accepted',
+                    'soj': soj,
+                    'sid': sid
+                }, sort=[('submittime', 1)]
+            )
+            if difficult_ac:
+                submission_id = difficult_ac.get('_id')
+                problem = yield self.settings['database']['problem'].find_one({'soj': soj, 'sid': sid})
+                title = ' {}'.format(problem.get('title')) if problem else ''
+                title = '{}-{}{}'.format(soj, sid, title)
 
-        difficult_ac = yield self.settings['database']['submission'].find_one(
-            {
-                'username': username,
-                'result': 'Accepted',
-                'soj': soj,
-                'sid': sid
-            }, sort=[('submittime', 1)]
-        )
-        submission_id = difficult_ac.get('_id')
-        problem = yield self.settings['database']['problem'].find_one({'soj': soj, 'sid': sid})
-        title = ' {}'.format(problem.get('title')) if problem else ''
-        title = '{}-{}{}'.format(soj, sid, title)
-
-        ret['none_ac_cnt'] = counter[max_none_ac_key]
-        ret['time'] = difficult_ac.get('submittime').strftime('%Y %m %d') if difficult_ac.get('submittime') else None
-        ret['title'] = title
-        ret['url'] = '/submission/{}'.format(submission_id) if submission_id else None
-        ret['code'] = difficult_ac.get('code')
+                ret['none_ac_cnt'] = counter[max_none_ac_key]
+                ret['time'] = difficult_ac.get('submittime').strftime('%Y %m %d') if difficult_ac.get('submittime') else None
+                ret['title'] = title
+                ret['url'] = '/submission/{}'.format(submission_id) if submission_id else None
+                ret['code'] = difficult_ac.get('code')
 
         return ret
 
@@ -334,6 +342,7 @@ class Handler(RequestHandler):
         ret['festival'] = festival
         #
         # TODO: 比赛排名
+
         contest = {
             "is_first": None,
             "is_last": None
@@ -343,7 +352,7 @@ class Handler(RequestHandler):
         return ret
 
     @gen.coroutine
-    def _get_contest_list(self, username):
+    def _get_contest_list(self):
         """
         :param username:
         :return:
@@ -365,6 +374,20 @@ class Handler(RequestHandler):
         # 你给我的比赛数据，要是根据比赛时间由近到远排序的，因为我们要优先展示最近的比赛
         # TODO: 添加字段rank
         contest_list = []
+        for contest_id in self.contest_id_list:
+            contest = yield self.settings['database']['contest'].find_one({'_id': contest_id})
+            contest_list.append({
+                'title': contest.get('title'),
+                'url': '/contest/entry/{}'.format(str(contest.get('_id'))),
+                'time': '{} ~ {}'.format(contest.get('begintime').strftime('%Y-%m-%d %H:%M:%S'),
+                                         contest.get('endtime').strftime('%Y-%m-%d %H:%M:%S')),
+                'description': contest.get('description')
+            })
+        ret = contest_list
+        return ret
+
+    @gen.coroutine
+    def _get_contest_id_list(self, username):
         contest_id_set = set()
         submissions = yield self.settings['database']['submission'].find(
             {
@@ -377,17 +400,7 @@ class Handler(RequestHandler):
                 # contest 不全
                 if (yield self.settings['database']['contest'].count({'_id': contest_id})):
                     contest_id_set.add(contest_id)
-        for contest_id in contest_id_set:
-            contest = yield self.settings['database']['contest'].find_one({'_id': contest_id})
-            contest_list.append({
-                'title': contest.get('title'),
-                'url': '/contest/entry/{}'.format(str(contest.get('_id'))),
-                'time': '{} ~ {}'.format(contest.get('begintime').strftime('%Y-%m-%d %H:%M:%S'),
-                                         contest.get('endtime').strftime('%Y-%m-%d %H:%M:%S')),
-                'description': contest.get('description')
-            })
-        ret = contest_list
-        return ret
+        return list(contest_id_set)
 
     def _get_cookie_username(self):
         username = self.get_secure_cookie("username")
