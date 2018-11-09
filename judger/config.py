c26ad2fa9f89e061c22fc5b1c1b412d367228db4
张家豪 <davidbill@outlook.com>
2018-11-09 23:43:45 +0800

    修复: 用户被ban

--- judger/config.py
+++ judger/config.py
@@ -38,26 +38,26 @@ account_list = {
         'nickname': 'sduwhvj',
     },
     'VJudge_list': [
-        {
-            'username': 'sduwhvj',
-            'password': 'sduwhvj2016',
-            'nickname': 'sduwhvj',
-        },
-        {
-            'username': 'sduwhvj2018',
-            'password': 'sduwhvj2018',
-            'nickname': 'sduwhvj2018',
-        },
-        {
-            'username': 'sduwhvjtest',
-            'password': 'sduwhvjtest',
-            'nickname': 'sduwhvjtest',
-        },
-        {
-            'username': 'sduwhvj0',
-            'password': 'sduwhvj0',
-            'nickname': 'sduwhvj0',
-        },
+        # {
+        #     'username': 'sduwhvj',
+        #     'password': 'sduwhvj2016',
+        #     'nickname': 'sduwhvj',
+        # },
+        # {
+        #     'username': 'sduwhvj2018',
+        #     'password': 'sduwhvj2018',
+        #     'nickname': 'sduwhvj2018',
+        # },
+        # {
+        #     'username': 'sduwhvjtest',
+        #     'password': 'sduwhvjtest',
+        #     'nickname': 'sduwhvjtest',
+        # },
+        # {
+        #     'username': 'sduwhvj0',
+        #     'password': 'sduwhvj0',
+        #     'nickname': 'sduwhvj0',
+        # },
         {
             'username': 'sduwhvj1',
             'password': 'sduwhvj1',
